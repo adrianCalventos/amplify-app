cd4b12920c835a2918409409d4a261bd76a7f384
adriancalventos <adriancalventosgarcia@gmail.com>
2023-09-12 19:32:24 +0200

    push changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "template": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "templatea718c61b": {
       "AppClientID": "string",
