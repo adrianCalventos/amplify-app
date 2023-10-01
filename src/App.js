8cedad97bbc12895c28ec87efa65faa1019ac722
adriancalventos <adriancalventosgarcia@gmail.com>
2023-10-02 00:50:10 +0200

    Revert "delete"
    This reverts commit e5ecda0c119e79d6d7b1ab6bd4379afe9f0ae59b.

--- src/App.js
+++ src/App.js
@@ -1,94 +1,24 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
 import {
-  Button,
-  Flex,
-  Heading,
-  TextField,
-  View,
   withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
-import Note from './notes/note';
-import { NavBarHeader2 } from "./ui-components";
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-  
-  useEffect(() => {
-    fetchNotes();
-  }, []);
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    setNotes(notesFromAPI);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-    };
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
-
+function App({ signOut }) {
   return (
     <View className="App">
-      <NavBarHeader2 login={withAuthenticator}/>
-
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) =>(  
-          <Note 
-            key={note.id}x
-            note={note}
-            notes={notes}
-            setNotes={setNotes}
-            id={note.id}
-            
-          />
-        ))}
-      </View>
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-};
+}
 
 export default App;
